16cb1cdc1e0af9d5979519a17dbec57d0264b2ef
BijonTalukder <bijontalukder1247@gmail.com>
2024-05-23 14:34:44 +0600

    dive driven GRAPHQL

--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,43 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { db } from "./db.js";
 const typeDefs = `#graphql
 
-  type Book {
-    title: String
-    author: String
+  type Product{
+    id: ID!,
+    name: String,
+    image: String,
+    description:
+    String,
+    price: Float,
+    quantity: Int,
+    onStock:Boolean ,
+    categoryId: String,
   }
 
   type Query {
-    books: [Book]
+  products:[Product]
+  product(productId:ID!):Product
   }
 `;
-const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
-];
+// const books = [
+//     {
+//       title: 'The Awakening',
+//       author: 'Kate Chopin',
+//     },
+//     {
+//       title: 'City of Glass',
+//       author: 'Paul Auster',
+//     },
+//   ];
 const resolvers = {
     Query: {
-        books: () => books,
+        products: () => db.products,
+        product: (parent, args, context) => {
+            const result = db.products.find((pd) => pd.id === args.productId);
+            return result;
+            console.log(parent, args, context);
+        },
     },
 };
 const server = new ApolloServer({
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,48 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { db } from "./db.js";
+import { log } from "console";
 
 const typeDefs = `#graphql
 
-  type Book {
-    title: String
-    author: String
+  type Product{
+    id: ID!,
+    name: String,
+    image: String,
+    description:
+    String,
+    price: Float,
+    quantity: Int,
+    onStock:Boolean ,
+    categoryId: String,
   }
 
   type Query {
-    books: [Book]
+  products:[Product]
+  product(productId:ID!):Product
   }
 `;
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
-
+// const books = [
+//     {
+//       title: 'The Awakening',
+//       author: 'Kate Chopin',
+//     },
+//     {
+//       title: 'City of Glass',
+//       author: 'Paul Auster',
+//     },
+//   ];
 
 const resolvers = {
-    Query: {
-      books: () => books,
+  Query: {
+    products: () => db.products,
+    product: (parent: any, args: { productId: string }, context: any) => {
+      const result = db.products.find((pd) => pd.id === args.productId);
+      return result;
+      console.log(parent, args, context);
     },
-  };
+  },
+};
 
 const server = new ApolloServer({
     typeDefs,
